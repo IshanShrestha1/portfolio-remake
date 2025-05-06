be852c355b517f7ace63f4ca28d8cd4a00780648
IshanShrestha1 <is.ishanshrestha@gmail.com>
2025-05-06 06:51:06 +0545

    small animation

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+// hamburger 
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+//text showing
+const text="Ishan Shrestha";
+const typedtext = document.getElementById('typed-text');
+let index=0;
+function type(){
+  if(index < text.length){
+    typedtext.textContent += text.charAt(index);
+    index++;
+    setTimeout(type,150);
+  }
+}
+type();
