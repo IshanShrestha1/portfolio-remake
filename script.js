38aca1410210d606b1ea31b62b2b930db8f3105f
IshanShrestha1 <is.ishanshrestha@gmail.com>
2025-05-08 07:29:57 +0545

    project using js

--- script.js
+++ script.js
@@ -17,3 +17,34 @@ function type(){
   }
 }
 type();
+
+// project section 
+const projects = [
+  {
+    Image: "images/HTML.jpg",
+    title: "Animated Navbar",
+    description: "A responsive navigation bar with smooth animation and toggle function",
+  },
+  {
+    Image: "images/HTML.jpg",
+    title: "Animated Navbar",
+    description: "A responsive navigation bar with smooth animation and toggle function",
+  },
+  {
+  Image: "images/HTML.jpg",
+    title: "Animated Navbar",
+    description: "A responsive navigation bar with smooth animation and toggle function",
+  },
+];
+const container = document.getElementById("project-container");
+
+  projects.forEach(project => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+    <img src="${project.Image}" alt="${project.title}"/>
+    <h2>${project.title}</h2>
+    <p>${project.description}</p>
+    `;
+    container.appendChild(card);
+  });
